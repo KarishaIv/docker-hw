d25078eef98accea9f91b042ac36b06c07f3b0df
Karina Ivanova <karinaivanova2005@inbox.ru>
2025-04-12 16:10:34 +0300

    add volumes for database

--- app/database.py
+++ app/database.py
@@ -1,21 +1,24 @@
 import sqlite3
+import os
+
+os.makedirs("data", exist_ok=True)
 
 def init_db():
-    conn = sqlite3.connect("data.db")
+    conn = sqlite3.connect("data/data.db")
     c = conn.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS urls (code TEXT PRIMARY KEY, url TEXT)")
     conn.commit()
     conn.close()
 
 def insert_url(code, url):
-    conn = sqlite3.connect("data.db")
+    conn = sqlite3.connect("data/data.db")
     c = conn.cursor()
     c.execute("INSERT INTO urls (code, url) VALUES (?, ?)", (code, url))
     conn.commit()
     conn.close()
 
 def get_url_by_code(code):
-    conn = sqlite3.connect("data.db")
+    conn = sqlite3.connect("data/data.db")
     c = conn.cursor()
     c.execute("SELECT url FROM urls WHERE code = ?", (code,))
     result = c.fetchone()
